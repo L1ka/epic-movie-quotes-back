5a427a16387117e132c3f825c9fc663ca37ecf09
L1ka <l.gvaramashvili1@gmail.com>
2023-07-14 00:33:40 +0400

    refactor: use global variables

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -35,21 +35,16 @@ class ResetPassword extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         $expires = Carbon::now('PST')->addMinutes(2);
+        $frontUrl = env('FRONT_URL');
+
         return (new MailMessage())
         ->from('no-reply@moviequotes.ge', 'Movie Quotes')
                     ->subject('Password reset link')
-        ->view('confirmation-password', ['url' => 'http://localhost:5173/new-password/?token='.$this->token.'&email='.request()->email.'&expires='.$expires, 'user'=> $notifiable->first_name]);
+        ->view('confirmation-password', [
+            'url' => $frontUrl.'/new-password/?token='.$this->token.'&email='.request()->email.'&expires='.$expires,
+            'user'=> $notifiable->first_name
+        ]);
     }
 
-    // /**
-    //  * Get the array representation of the notification.
-    //  *
-    //  * @return array<string, mixed>
-    //  */
-    // public function toArray(object $notifiable): array
-    // {
-    //     return [
-    //         //
-    //     ];
-    // }
+
 }
