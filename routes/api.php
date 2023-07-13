f09f9de78511491bcf7a353b08bbde1d1600c787
L1ka <l.gvaramashvili1@gmail.com>
2023-07-14 00:34:03 +0400

    refactor: routes

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,18 @@
 <?php
 
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleAuthController;
 use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\InteractionController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\UserProfileController;
-use App\Http\Resources\MovieResource;
-use App\Http\Resources\QuoteResource;
-use App\Http\Resources\UserResource;
-use App\Models\Genre;
-use App\Models\Movie;
-use App\Models\Quote;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Mockery\Mock;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,89 +24,73 @@ use Mockery\Mock;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
 
 
-Route::post('/email-verified', [AuthController::class, 'verify'])
-    ->name('email-verification');
+Route::group(['controller' => AuthController::class], function () {
+    Route::post('register',  'register')->name('register');
+    Route::post('login',  'login')->name('login');
+    Route::post('logout',  'logout')->name('logout');
+    Route::post('/email-verified',  'verify')->name('email-verification');
+    Route::post('/send-email',  'sendEmail')->name('send-email');
+    Route::get('/user',  'authUser')->name('get-user');
+});
 
-Route::post('/send-email', [AuthController::class, 'sendEmail'])
-    ->name('send-email');
-
-
-
-Route::post('/forgot-password', [ResetPasswordController::class, 'resetEmail'])->name('reset-password');
-Route::post('/reset-password', [ResetPasswordController::class, 'update'])->name('password.update');
-
-
-
-Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->name('google-redirect');
-
-Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])->name('google-callback')->middleware('web');
-
-
-Route::post('/user', function () {
-
-   // return response()->json(['user' => Auth::user()]);
-   return new UserResource(Auth::user());
+Route::group(['controller' => ResetPasswordController::class], function () {
+    Route::post('/forgot-password', 'resetEmail')->name('reset-password');
+    Route::post('/reset-password', 'update')->name('password.update');
 });
 
 
-Route::post('/update-user', [UserProfileController::class, 'updateUser'])->name('update-user');
-
-Route::post('/upload-image', [UserProfileController::class, 'uploadImage'])->name('upload-image');
-
-
-Route::get('/get-genres', function () {
-
-    return response()->json(['genres' => Genre::all()]);
-
-});
-
-Route::post('/movie/create', [MovieController::class, 'create'])->name('movie.create');
-Route::post('/movie/update', [MovieController::class, 'update'])->name('movie.update');
-Route::post('/movie/delete', [MovieController::class, 'delete'])->name('movie.delete');
-
-
-Route::post('/get-movie', function (Request $request) {
-
-    $movie = Movie::where('id', $request->input('id'))
-    ->first();
-
-    return new MovieResource($movie);
+Route::group(['controller' => GoogleAuthController::class], function () {
+    Route::get('/auth/google/redirect', 'redirect')->name('google-redirect');
+    Route::get('/auth/google/callback', 'callback')->name('google-callback')->middleware('web');
 });
 
 
-Route::get('/get-movies', function (Request $request) {
 
-    app()->setLocale($request->getPreferredLanguage());
+Route::middleware('auth:sanctum')->group(function() {
+    Route::group(['controller' => QuoteController::class], function () {
+        Route::post('quote/store',  'store')->name('quote.store');
+        Route::post('quote/update',  'update')->name('quote.update');
+        Route::delete('quote/delete/{id}',  'delete')->name('quote.delete');
+        Route::get('get-quote/{id}',  'getQuote')->name('get-quote');
+        Route::get('get-quotes/{id}',  'getQuotes')->name('get-quotes');
+    });
 
-   return MovieResource::collection(Movie::all());
+    Route::group(['controller' => MovieController::class], function () {
+        Route::post('/movie/create', 'create')->name('movie.create');
+        Route::post('/movie/update','update')->name('movie.update');
+        Route::delete('/movie/delete/{id}',  'delete')->name('movie.delete');
+        Route::get('/get-movie/{id}',  'getMovie')->name('get-movie');
+        Route::get('/get-movies',  'getMovies')->name('get-movies');
+        Route::get('/get-genres',  'getGenres')->name('get-genres');
+    });
+
+
+
+    Route::group(['controller' => InteractionController::class], function () {
+        Route::post('create-comment', 'addComment')->name('create-comment');
+        Route::post('create-like', 'addLike')->name('create-like');
+        Route::get('get-notifications', 'getNotifications')->name('get-notifications');
+        Route::post('notification-seen', 'notificationSeen')->name('notification-seen');
+        Route::post('mark-all-seen', 'MarkAllSeen')->name('mark-all-seen');
+    });
+
+
+    Route::group(['controller' => UserProfileController::class], function () {
+        Route::post('/update-user',  'updateUser')->name('update-user');
+        Route::post('/upload-image',  'uploadImage')->name('upload-image');
+        Route::post('/update-email',  'updateEmail')->name('update-email');
+    });
+
+    Route::group(['controller' => SearchController::class], function () {
+        Route::post('search',  'search')->name('search');
+        Route::post('search-my-movies',  'searchMyMovies')->name('search-my-movies');
+    });
+
+
+
 });
 
-Route::post('/get-quotes', function (Request $request) {
-
-    app()->setLocale($request->getPreferredLanguage());
-
-    $quotes = Movie::where('id', $request->input('id'))
-    ->first()->quotes;
-
-    return QuoteResource::collection($quotes);
-});
-
-Route::post('/get-quote', function (Request $request) {
-
-    app()->setLocale($request->getPreferredLanguage());
-
-    $quote = Quote::where('id', $request->input('id'))
-    ->first();
-
-    return new QuoteResource($quote);
-});
 
 
-Route::post('/quote/create', [QuoteController::class, 'create'])->name('quote.create');
-Route::post('/quote/update', [QuoteController::class, 'update'])->name('quote.update');
-Route::post('/quote/delete', [QuoteController::class, 'delete'])->name('quote.delete');
