1d90c9133a7cf5249b96d67f67301069ecd94394
L1ka <l.gvaramashvili1@gmail.com>
2023-07-14 00:30:25 +0400

    chore: movie and profile requests

---
+++ app/Http/Requests/Movie/MovieRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Movie;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+class MovieRequest extends FormRequest
+{
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(Request $request): array
+    {
+        app()->setLocale($request->getPreferredLanguage());
+        return [
+            'title' => ['required', 'unique:movies,title->en', 'unique:movies,title->ka'],
+            'year' => ['required',  'digits:4'],
+            'director' => ['required'],
+            'image' => ['required'],
+        ];
+    }
+}
--- app/Http/Requests/Profile/UserProfileRequest.php
+++ app/Http/Requests/Profile/UserProfileRequest.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App\Http\Requests\Comment;
+namespace App\Http\Requests\Profile;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CommentRequest extends FormRequest
+class UserProfileRequest extends FormRequest
 {
-
 
     /**
      * Get the validation rules that apply to the request.
@@ -16,10 +15,8 @@ class CommentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'body' =>  'required',
-            'user_id' => 'required',
-            'quote_id' => 'required',
-            'movie_id' => 'required'
+            'email' => ['email','unique:users'],
+            'first_name' => [ 'unique:users'],
         ];
     }
 }
